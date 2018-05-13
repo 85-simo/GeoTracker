8c946de8fc7934a546a173e9a302b4f501fc5847
Simone Bernardini <simoneb966@gmail.com>
2018-05-13 20:17:29 +0200

    Defined Main Activity structure+viewmodel

--- app/src/main/java/com/example/geotracker/presentation/map/MapViewModelModule.java
+++ app/src/main/java/com/example/geotracker/presentation/map/MapViewModelModule.java
@@ -1,10 +1,21 @@
 package com.example.geotracker.presentation.map;
 
+import android.arch.lifecycle.ViewModel;
+
+import com.example.geotracker.presentation.PerActivity;
+import com.example.geotracker.presentation.ViewModelKey;
 import com.example.geotracker.presentation.ViewModelModule;
 
+import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.IntoMap;
 
-@Module(includes = {ViewModelModule.class})
+@Module
 public abstract class MapViewModelModule {
-    // TODO
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MapViewModel.class)
+    @PerActivity
+    abstract ViewModel bindMapViewModel(MapViewModel mapViewModel);
 }
